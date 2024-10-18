7bbf275b0cf27f1d83115c742a6625c4d4543498
ludo77100 <ludovic.s@protonmail.com>
2024-10-18 16:36:26 +0200

    Ajout des premier tests et d'une fixture pour la liste des groupes de spécialités et spécialités

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,38 @@
+describe('Test de login', () => {
+  it('Se rendre au point d\'entrée de l\'application et entrer les credentials', () => {
+    // Se rendre sur la page de login
+    cy.visit('http://localhost:4200/login')
+
+    // Remplir l'email
+    cy.get('input#email').type('super.admin@email.com')
+
+    // Remplir le mot de passe
+    cy.get('input#password').type('123456')
+
+    // Cliquer sur le bouton de login
+    cy.get('button[type="submit"]').click()
+
+    // Vérifier que la page s'affiche bien après la connexion et se rendre sur la page "New Emergency"
+    cy.get('#new-emergency-btn').click()
+  
+    // Remplir la latitude
+    cy.get('input#userLat').type('51.52927577065579')
+
+    // Remplir la longitude
+    cy.get('input#userLon').type('-0.11747458456620688')
+
+    // Sélectionner un groupe de spécialité dans la liste déroulante
+    cy.get('select#specialityGroupId').select('15')
+
+    // Sélectionner une spécialité dans la liste déroulante
+    cy.get('select#specialityId').select('38') 
+    
+    // Soumettre le formulaire
+    cy.get('button[type="submit"]').click()
+
+    // Vérifier la présence du texte " British Home & Hospital for Incurables " sur la page
+    cy.contains(' British Home & Hospital for Incurables')
+
+  })
+})
+
